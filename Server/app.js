b85fa621cf330a40f8cae32b06d308f9724befda
buse506 <032290055@ogr.uludag.edu.tr>
2025-12-25 15:27:37 +0300

    Proje yapısı Express ve EJS'ye taşındı, klasör düzeni güncellendi

---
+++ Server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// EJS'yi şablon motoru olarak ayarla
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// CSS, JS ve Resimleri 'public' klasöründen çekmesini söyle
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ANA SAYFA (index.ejs'yi açar)
+app.get('/', (req, res) => {
+    res.render('index'); 
+});
+
+// HAKKIMDA SAYFASI (hakkimda.ejs'yi açar)
+app.get('/hakkimda', (req, res) => {
+    res.render('hakkimda');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Sunucu http://localhost:${PORT} adresinde aktif!`);
+});
